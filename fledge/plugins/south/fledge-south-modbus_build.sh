be1f907af96ec9dd626a176844d037dfda2c47b7
skumarorxa <skumar@orxagrid.com>
2025-01-31 08:59:26 +0530

    integration of important plugins for all fledge services

---
+++ fledge/plugins/south/fledge-south-modbus_build.sh
@@ -0,0 +1,10 @@
+FLEDGENOTIFVERSION=2.6.0
+RELEASE=2.6.0
+OPERATINGSYSTEM=ubuntu2004
+ARCHITECTURE=x86_64
+FLEDGELINK="http://archives.fledge-iot.org/$RELEASE/$OPERATINGSYSTEM/$ARCHITECTURE"
+
+wget --no-check-certificate ${FLEDGELINK}/fledge-south-modbus_${FLEDGENOTIFVERSION}_${ARCHITECTURE}.deb
+dpkg --unpack ./fledge-south-modbus_${FLEDGENOTIFVERSION}_${ARCHITECTURE}.deb
+apt-get install -yf
+apt-get clean -y

2e91b6c47bd5c2592fa9779924ae63b9ca2e63cc
skumarorxa <skumar@orxagrid.com>
2025-02-18 16:30:33 +0530

    updated httpc shell script

--- fledge/plugins/north/fledge-north-http-c_build.sh
+++ fledge/plugins/north/fledge-north-http-c_build.sh
@@ -1,14 +1,10 @@
-git clone https://github.com/fledge-iot/fledge-north-http-c.git
-cd fledge-north-http-c
-chmod +x mkversion
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DFLEDGE_INCLUDE=/usr/local/fledge/include/ -DFLEDGE_LIB=/usr/local/fledge/lib/ ..
-make
-if [ ! -d "${FLEDGE_ROOT}/plugins/north/http-c" ] 
-then
-    sudo mkdir -p $FLEDGE_ROOT/plugins/north/http-c
-fi
-sudo cp libhttpc.so $FLEDGE_ROOT/plugins/north/http-c
-# Rename the file after copying
-sudo mv $FLEDGE_ROOT/plugins/north/http-c/libhttpc.so $FLEDGE_ROOT/plugins/north/http-c/libhttp-c.so
+FLEDGENOTIFVERSION=2.6.0
+RELEASE=2.6.0
+OPERATINGSYSTEM=ubuntu2004
+ARCHITECTURE=x86_64
+FLEDGELINK="http://archives.fledge-iot.org/$RELEASE/$OPERATINGSYSTEM/$ARCHITECTURE"
+
+wget --no-check-certificate ${FLEDGELINK}/fledge-north-httpc_${FLEDGENOTIFVERSION}_${ARCHITECTURE}.deb
+dpkg --unpack ./fledge-north-httpc_${FLEDGENOTIFVERSION}_${ARCHITECTURE}.deb
+apt-get install -yf
+apt-get clean -y
